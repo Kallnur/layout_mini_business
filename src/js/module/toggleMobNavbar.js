3b9225b753e0769142991298dc13218d1910d344
Kalnur <kalnur.go@gmail.com>
2022-06-23 13:35:56 +0600

    add services detail page

--- src/js/module/toggleMobNavbar.js
+++ src/js/module/toggleMobNavbar.js
@@ -9,13 +9,11 @@ export const toggleMobNavbar = () => {
         btn.addEventListener('click', () => {
             if(!btnsBurger[0].classList.contains(btnActiveClass)){
                 btnsBurger[0].classList.add(btnActiveClass);
-                btnsBurger[1].classList.add(btnActiveClass);
                 mobNavbar.classList.add(navActiveClass);
                 document.body.style.overflow = 'hidden';
             }
             else{
                 btnsBurger[0].classList.remove(btnActiveClass);
-                btnsBurger[1].classList.remove(btnActiveClass);
                 mobNavbar.classList.remove(navActiveClass);
                 document.body.style.overflow = '';
             }
